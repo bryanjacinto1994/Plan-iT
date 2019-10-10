d2c3a7df9537641e22c8b3f853f39158517d8b1a
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-10 09:30:17 -0700

    Made a createConnection function to hold the host,port,user,pass,database information

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,9 @@
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'planner_db'
+});
