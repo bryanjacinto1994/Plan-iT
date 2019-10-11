780191f6034a715077f1a3bd28fd4df0fd833694
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-10 17:47:27 -0700

    Added INSERT and VALUES in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO planners (plan_name, completed)
+VALUES ('Clean Room', false);
+
+INSERT INTO planners (plan_name, completed)
+VALUES ('Study JavaScript', false);
+
+INSERT INTO planners (plan_name, completed)
+VALUES ('Fix Resume', false);
+
+INSERT INTO planners (plan_name, completed)
+VALUES ('Clean Garage', false);
+
