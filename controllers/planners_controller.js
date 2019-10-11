4afe2acda66be1e6d061369b21cca32cd79e793e
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-10 17:15:07 -0700

    Made a get request using express function to select all data

--- controllers/planners_controller.js
+++ controllers/planners_controller.js
@@ -4,3 +4,13 @@ var router = express.Router();
 
 var planner = require('../models/planners.js');
 
+router.get('/', function(req, res){
+    planner.selectAll(function(data){
+        var hbsObject = {
+            planners: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
